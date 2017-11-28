98cd8e810ffe2114a0b9218de8950b8c39359f91
mauricio <mauricio@DESKTOP-1PIPS59>
2017-11-28 10:01:57 +0000

    Clasificacion a partir de puntos de cada jornada

---
+++ src/modelo/Equipo.java
@@ -0,0 +1,23 @@
+package modelo;
+
+public class Equipo {
+	private String nombre;
+	private int puntos;
+	
+	public Equipo() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getPuntos() {
+		return puntos;
+	}
+	public void setPuntos(int puntos) {
+		this.puntos = puntos;
+	}
+}
